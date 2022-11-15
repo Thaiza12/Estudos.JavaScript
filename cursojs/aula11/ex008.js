fc5c7eada227d697bf97dda674825fa1052d179b
Thaiza Alves <87837910+Thaiza12@users.noreply.github.com>
2022-11-15 20:16:02 -0300

    Add files via upload
    Atualizações de exercícios

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(` A velocidade do carro ${vel}KM/h`)
+if(vel > 60) { // condição simple é quando se usa apenas o if 
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+} 
+console.log(`Dirija sempre usando o sinto de segurança!`)
---
+++ cursojs/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log (`Vivendo em ${país}`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{ // condição composta, pois usa o if e else
+    console.log('Você é Estrangeiro!')
+}
