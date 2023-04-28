3a8a3e4cf36528f40db07479b5dbea257f675bd5
Thaiza Alves <87837910+Thaiza12@users.noreply.github.com>
2023-04-28 11:27:59 -0300

    Add files via upload

--- cursojs/ex018/script.js
+++ cursojs/ex018/script.js
@@ -1,11 +1,34 @@
 
-    let num = window.document.getElementById('txtn')
-    let lista = window.document.getElementById('lista')
-    let res = window.document.getElementById('res')
+    let num = window.document.querySelector('input#fnum')
+    let lista = window.document.querySelector('select#lista')
+    let res = window.document.querySelector('div#res')
     let valores= []
-    function adicionar() {
-        if (isNumero(num.value)&& !inLista(num.value)){
-        } else{
-            window.alert(' Valor inválido ou já está na lista ')
+    function isNumero(n){
+        if(Number(n)>= 1 && Number(n)<=100){
+            return true
+        }else{
+            return false
         }
+        
+    }
+    
+    function inLista(n, l){
+        if(l.indexOf(Number(n)) != -1){// -1 significa que n tem esse elemento no array
+            window.alert(`O valor já foi adicionado ou inexistente`)
+            
+            return true
+        }else{
+            return false
+        }
+    }
+    function adicionar(){
+        if(isNumero(num.value) && !inLista(num.value, valores)){// O ! antes de Lista e para dizer se o elemento estiver dentro do array = false, se n estiver = true
+            valores.push(Number(num.value))
+            let item = document.createElement('option')
+            item.text = `Valor ${num.value} adicionado.` //.TEXT PARA ESCREVAR NA TELA
+            lista.appendChild(item)
+            res.innerHTML = ''
+        }
+        num.value ='' //limpar o valor digitado na barra
+        num.focus() //pra deixar o campo selecionado
     }
